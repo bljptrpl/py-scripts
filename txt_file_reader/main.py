95fe115a9595ac6d619b86145bf26b77b5814e01
Dan <79495801+dvillalva21@users.noreply.github.com>
2022-11-21 18:17:24 -0700

    seperation by spaces

--- txt_file_reader/main.py
+++ txt_file_reader/main.py
@@ -8,12 +8,7 @@ new = []
 for line in rl: 
     new.append(line.strip())
 
-    # if line[-1] == '\n':
-    #     new.append(line[:-1])
-    # else
-    #     new.append(line)
 file.close
-#print(new)
 
 #convert list into string
 k = str(new)
@@ -25,18 +20,20 @@ num_tracker = num_from
 num_where = k.find('where')
 print(f'Select:{num_select} From:{num_from} Where:{num_where}')
 #find comma's between from and where 
-comma = []
-print(num_tracker) #79
-
-while (k.find(',', num_tracker)) < num_where:
-    num_tracker = k.find(',', num_tracker)
-    comma.append(num_tracker)
-    print(f'Comma List:{comma}')
+spaces = []
+while (k.find(' ', num_tracker)) < num_where:
+    num_tracker = k.find(' ', num_tracker)
+    spaces.append(num_tracker)
+    print(f'Spaces List:{spaces}')
     num_tracker += 1
-    # if (k.find(',', num_tracker)) > num_where:
-    #     break
-
-print(k[79:187])
-
-print("ye")
+i = 0
+p = 1
+#keyu = []
+print(len(spaces))
+for i in range(len(spaces)):
+    print(k[spaces[i]:spaces[p]])
+    #keyu[i] = k[spaces[j]:spaces[p]]
+    p += 1
+ 
+ #print(keyu)
 # strip() 
