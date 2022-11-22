e39423c4dd9a14ef5149fe6047122178514fe67c
Dan <79495801+dvillalva21@users.noreply.github.com>
2022-11-22 09:36:40 -0700

    Removing prints

--- txt_file_reader/main.py
+++ txt_file_reader/main.py
@@ -18,25 +18,22 @@ num_select = k.find('select')
 num_from = k.find('from')
 num_tracker = num_from
 num_where = k.find('where')
-print(f'Select:{num_select} From:{num_from} Where:{num_where}')
+#print(f'Select:{num_select} From:{num_from} Where:{num_where}')
 #find space between from and where 
 spaces = []
 while (k.find(' ', num_tracker)) < num_where:
     num_tracker = k.find(' ', num_tracker)
     spaces.append(num_tracker)
-    print(f'Spaces List:{spaces}')
+    #print(f'Spaces List:{spaces}')
     num_tracker += 1
 
 #Store the strings between spaces
 i = 0
 p = 1
 keyu = []
-print(len(spaces))
 for i in range(len(spaces)-1):
-    print(k[spaces[i]:spaces[p]])
     keyu.append(k[spaces[i]:spaces[p]])
     p += 1
     print(keyu[i])
- 
 
 # strip()
