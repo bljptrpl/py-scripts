4ad47acd7a23b04f5374dd0de4b7243548be0f0c
Dan <79495801+dvillalva21@users.noreply.github.com>
2022-11-28 17:20:10 -0700

    learning search funct

--- pandas/pokimon_ex.py
+++ pandas/pokimon_ex.py
@@ -1,8 +1,10 @@
 import pandas as pd
+# https://www.youtube.com/watch?v=vmEHCJofslg&ab_channel=KeithGalli
 
 df = pd.read_csv('C:\\Users\\dvillalva\\Desktop\\scripts\pandas\\pokemon_data.csv')
-print(df.head(3))
+#print(df.head(3))
 df.to_csv('C:\\Users\\dvillalva\\Desktop\\scripts\pandas\\modified.csv', sep='\t')
 # df.to_excel('modified.xlsx', index=False)
 print('test')
 
+df.loc[df['Type 1'] == 'Flamer', 'Type 1'] = 'Fire'
